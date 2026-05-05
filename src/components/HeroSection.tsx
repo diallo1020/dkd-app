c9cae2f6d3258679d23a066fc033373549da29c1
diallo1020 <djenaboudkd@gmail.com>
2026-05-05 22:06:42 +0000

    update

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,20 +2,21 @@
 const HeroSection = () => {
   return (
     <>
-       <div className='grid grid-cols-2 p-4 bg-primary/20  items-center gap-4'>
-      <div className='flex flex-row gap-2 lg:p-16 md:p-4'>
+       <div className='grid grid-cols-3 p-4 bg-primary/20  items-center gap-4 h-80 overflow-hidden'>
+      <div className='flex flex-row gap-2 lg:p-16 md:p-4 bg-primary col-span-1'>
         <div className='flex flex-col gap-4'>
-        <h1 className="font-bold text-4xl">Sublimez votre élégance</h1>
+        <h1 className="font-bold text-4xl md:text-xl">Sublimez votre élégance</h1>
         <p>Bijoux raffinés, montres élégantes, sacs tendance <br />
         et chaussures de luxe pour la femme moderne.</p>
        
-        <div className='flex md:flex-row gap-2'>
+        <div className='flexflex-row md:flex-col gap-2'>
             <button className='btn btn-primary text-white'>Découvrir la collection</button>
              <button className='btn btn-secondary/300  text-primary border border-primary/50'>Nous contacter</button>
         </div>
          </div>
       </div>
-      <img src="/images/hero.png" alt="hero" />
+      <img src="/images/hero.png" alt="hero" 
+      className="w-full h-80 object-cover bg-secondary col-span-2 " />
     </div>
     </>
   )
