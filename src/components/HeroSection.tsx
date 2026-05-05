61aa00c294e23233b83ed1d7345bac0ba7618126
diallo1020 <djenaboudkd@gmail.com>
2026-05-05 22:27:03 +0000

    force deploy

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,17 +2,17 @@
 const HeroSection = () => {
   return (
     <>
-       <div className='grid grid-cols-2 p-4 bg-primary/20  items-center gap-4 h-80 overflow-hidden'>
+       <div className='grid grid-cols-2 p-2 bg-primary/20  items-center lg:gap-4 gap-2 h-80 overflow-hidden'>
       <div className='flex flex-row gap-2 lg:p-16 md:p-4  '>
         <div className='flex flex-col gap-2'>
         <h1 className="font-bold lg:text-4xl text-2xl">Sublimez votre élégance</h1>
         <p className="text-sm lg:text-xl">Bijoux raffinés, montres élégantes, sacs tendance <br />
         et chaussures de luxe pour la femme moderne.</p>
        
-        <div className='flex lg:flex-row flex-col gap-2 '>
+       
             <button className='btn btn-primary text-white '>Découvrir la collection</button>
              <button className='btn btn-secondary/300  text-primary border border-primary/50'>Nous contacter</button>
-        </div>
+       
          </div>
       </div>
       <img src="/images/hero.png" alt="hero" 
